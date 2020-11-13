1ebe4668609279fb7e83a4928b61dd82d0bcf28e
Quinten <tintin10q@hotmail.com>
2020-11-13 18:58:35 +0100

    Added web server test holy dam that post request was hard 🎄

---
+++ tests/test_web_server.py
@@ -0,0 +1,37 @@
+from web_frontend import app
+import unittest
+from werkzeug.datastructures import ImmutableMultiDict, FileStorage
+import io
+import os
+
+print(os.getcwd())
+
+
+# ImmutableMultiDict([('audio_file', <FileStorage: 'harvard.wav' ('audio/wav')>)])
+# ImmutableMultiDict([('audio_file', FileStorage(io.open("harvard.wav"), "harvard.wav"))])
+
+class TestWebServer(unittest.TestCase):
+    # <FileStorage: 'harvard.wav' ('audio/wav')>
+    def setUp(self):
+        app.testing = True
+        app.debug = True
+        self.app = app.test_client()
+
+    def test_root_get(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200, "Get of / did not return 200")
+
+    def test_root_post(self):
+        print("This is an integration test testing everything!")
+        file = FileStorage(
+            stream=open("harvard.wav", "rb", buffering=0),
+            filename="harvard.wav",
+            content_type="audio/wav",
+        )
+        data = {"audio_file": file}
+        result = self.app.post('/', content_type='multipart/form-data', data=data)
+        self.assertEqual(result.status_code, 200, "Post of / did not return 200")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- web_frontend/blueprints/root.py
+++ web_frontend/blueprints/root.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, flash
-import speech_recognition as sr
+import available_models as am
 from model_server import translate_server
-import json
+
 
 root_blueprint = Blueprint('root', __name__)
 
@@ -11,13 +11,9 @@ def root_get():
     return render_template("root.html")
 
 
-
-
-
 @root_blueprint.route("/", methods=["POST"])
 def root_post():
     file = request.files['audio_file']
     text = translate_server.transcribe_audio(file, "sphinx")
-    translated = translate_server.translate_text(text, url=json.load(open("available_models/conf.json"))[
-        "translate_url"], model_id=100)
+    translated = translate_server.translate_text(text, url=am.get_conf()["translate_url"], model_id=100)
     return render_template("root.html", text=translated.src, text_t=translated.tgt, score=translated.score)
